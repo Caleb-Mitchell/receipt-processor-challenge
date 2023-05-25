5cd5c72088b7f3c2e279f62239a464d68205182b
Caleb-Mitchell <63262011+Caleb-Mitchell@users.noreply.github.com>
2023-05-25 16:01:54 -0500

    Finish writing tests

--- test/receipt_processor_test.rb
+++ test/receipt_processor_test.rb
@@ -1,5 +1,4 @@
 require 'simplecov'
-
 SimpleCov.start
 
 ENV["RACK_ENV"] = "test"
@@ -8,6 +7,7 @@ require "minitest/autorun"
 require "rack/test"
 
 require_relative "../receipt_processor"
+require_relative "sample_test_data"
 
 class MusicnotesTest < Minitest::Test
   include Rack::Test::Methods
@@ -16,40 +16,113 @@ class MusicnotesTest < Minitest::Test
     Sinatra::Application
   end
 
-  # def setup
-  # end
-  #
-  # def teardown
-  # end
-
   def session
     last_request.env["rack.session"]
   end
 
-  ### tests for POST request
-  # test that id is stored in session
+  def test_no_payload_present
+    post "/receipts/process"
+    assert_equal 400, last_response.status
+  end
 
-  # test that points are stored in session
+  def test_target_payload_present
+    post "/receipts/process", TEST_PAYLOAD_TARGET.to_json,
+         "CONTENT_TYPE" => "application/json"
+    assert_equal 200, last_response.status
+  end
 
-  # test that points are correct for an example receipt
-  # (test both examples given)
+  def test_target_payload_json
+    post "/receipts/process", TEST_PAYLOAD_TARGET.to_json,
+         "CONTENT_TYPE" => "application/json"
+    assert JSON.parse(last_response.body)
+  end
 
-  # test that response is a JSON object
+  def test_target_response_headers
+    post "/receipts/process", TEST_PAYLOAD_TARGET.to_json,
+         "CONTENT_TYPE" => "application/json"
+    assert_equal "application/json", last_response["Content-Type"]
+  end
 
-  # test that successful status code is given on a success
-  # (this is based on api example)
+  def test_target_id_returned
+    post "/receipts/process", TEST_PAYLOAD_TARGET.to_json,
+         "CONTENT_TYPE" => "application/json"
+    uuid_regexp = %r(\A[a-f0-9]{8}-[a-f0-9]{4}-4[a-f0-9]{3}-
+                [89ab][a-f0-9]{3}-[a-f0-9]{12}\z)ix
+    assert_match uuid_regexp, JSON.parse(last_response.body)["id"]
+    assert_equal "id", JSON.parse(last_response.body).keys.first
+  end
 
-  # test that unseccuessful status code is given on a failure
-  # (this is based on api example)
+  def test_id_in_session
+    post "/receipts/process", TEST_PAYLOAD_TARGET.to_json,
+         "CONTENT_TYPE" => "application/json"
+    assert_includes session.keys, JSON.parse(last_response.body)["id"]
+  end
 
-  ### tests for GET request
-  # test that correct response is given for both examples
+  def test_target_points
+    post "/receipts/process", TEST_PAYLOAD_TARGET.to_json,
+         "CONTENT_TYPE" => "application/json"
+    assert_includes session.values, 28
+  end
 
-  # test that response is a JSON object
+  def test_m_and_m_points
+    post "/receipts/process", TEST_PAYLOAD_M_AND_M.to_json,
+         "CONTENT_TYPE" => "application/json"
+    assert_includes session.values, 109
+  end
 
-  # test that successful status code is given on a success
-  # (this is based on api example)
+  def test_corner_store_points
+    post "/receipts/process", TEST_PAYLOAD_CORNER_STORE.to_json,
+         "CONTENT_TYPE" => "application/json"
+    assert_includes session.values, 18
+  end
 
-  # test that unseccuessful status code is given on a failure
-  # (this is based on api example)
+  def test_successful_point_retrieval
+    post "/receipts/process", TEST_PAYLOAD_TARGET.to_json,
+         "CONTENT_TYPE" => "application/json"
+    receipt_id = JSON.parse(last_response.body)["id"]
+
+    get "/receipts/#{receipt_id}/points"
+    assert_equal 200, last_response.status
+  end
+
+  def test_correct_points_retrieved_target
+    post "/receipts/process", TEST_PAYLOAD_TARGET.to_json,
+         "CONTENT_TYPE" => "application/json"
+    receipt_id = JSON.parse(last_response.body)["id"]
+
+    get "/receipts/#{receipt_id}/points"
+    assert_equal 28, JSON.parse(last_response.body)["points"]
+  end
+
+  def test_correct_points_retrieved_m_and_m
+    post "/receipts/process", TEST_PAYLOAD_M_AND_M.to_json,
+         "CONTENT_TYPE" => "application/json"
+    receipt_id = JSON.parse(last_response.body)["id"]
+
+    get "/receipts/#{receipt_id}/points"
+    assert_equal 109, JSON.parse(last_response.body)["points"]
+  end
+
+  def test_correct_points_retrieved_corner_store
+    post "/receipts/process", TEST_PAYLOAD_CORNER_STORE.to_json,
+         "CONTENT_TYPE" => "application/json"
+    receipt_id = JSON.parse(last_response.body)["id"]
+
+    get "/receipts/#{receipt_id}/points"
+    assert_equal 18, JSON.parse(last_response.body)["points"]
+  end
+
+  def test_response_json_object
+    post "/receipts/process", TEST_PAYLOAD_TARGET.to_json,
+         "CONTENT_TYPE" => "application/json"
+    receipt_id = JSON.parse(last_response.body)["id"]
+
+    get "/receipts/#{receipt_id}/points"
+    assert JSON.parse(last_response.body)
+  end
+
+  def test_no_receipt_found_for_id
+    get "/receipts/invalid-id/points"
+    assert_equal 404, last_response.status
+  end
 end
